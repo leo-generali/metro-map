70c57e49a4ff02e146223c4476c7914a9b8f0270
Leo Generali <leogen17@gmail.com>
2017-01-04 23:33:49 -0500

    Use IIFE to correctly display all train names and lines

--- js/app.js
+++ js/app.js
@@ -25,30 +25,59 @@
 
 //Set up the Map
 
-var map;
+var markers = [];
 
-var testObj = {lat: 38.8951100, lng: -77.0363700};
+function initializeMap() {
+      //Creates the map of the DMV
+      var centerLat = 38.8951100;
+      var centerLng = -77.0363700;
+      var center = new google.maps.LatLng(centerLat, centerLng);
 
-function initMap() {
-   	map = new google.maps.Map(document.getElementById('map'), {
-   		center: {lat: 38.8951100, lng: -77.0363700},
-   		zoom: 12
-   	});
+      var options = {
+         'zoom': 12,
+         'center': center
+      };
 
+      var map = new google.maps.Map(document.getElementById('map'), options);
+
+      //Station Information
+      //var i = 0;
+
+
+
+
+      //Populates the metro map with every station
    	function populateMap(num){
-   		for(var i = 0; i < num; i++){
-   			testObj.lat = model.Stations[i].Lat;
-   			testObj.lng = model.Stations[i].Lon;
-			var marker = new google.maps.Marker({
-				position: testObj,
-				map: map,
-			    title: model.Stations[i].Name
-		  	});
-   		}
+         for(var i = 0; i < num; i++){
+            (function(current){
+
+               //Creates text with information on the Stations name and lines that go through it
+               var contentString = '<b>Station: </b>' + model.Stations[current].Name + '<br>' + '<b>Line: </b>' + model.Stations[current].LineCode1;
+
+               var infowindow = new google.maps.InfoWindow({
+                  content: contentString
+               });
+
+               console.log(model.Stations[current].Name);
+
+
+               //Places marker on map
+               var latLng = new google.maps.LatLng(model.Stations[current].Lat, model.Stations[current].Lon);
+               var marker = new google.maps.Marker({
+                  position: latLng,
+                  map: map,
+                  title: model.Stations[current].Name
+               });
+
+               //Opens infowindow containing contentString when the marker is clicked
+               marker.addListener('click', function(){
+                  infowindow.open(map, marker);
+               });
+
+               markers.push(marker);
+
+            })(i)
+         }
    	}
-
    	populateMap(model.Stations.length);
-
 }
-
-initMap();	
