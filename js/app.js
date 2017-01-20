14c74c70d1631c30152daaa98f711c48ff7947fe
Leo Generali <leogen17@gmail.com>
2017-01-19 23:17:25 -0500

    More styling updates to the metro names

--- js/app.js
+++ js/app.js
@@ -53,15 +53,14 @@ function initializeMap() {
                //Creates text with information on the Stations name and lines that go through it
                var contentString = 
                '<p class="stationName">' + marker.title + '</p>' + '<div id="content">' + 
-               '<p class="stationLine ' + model.Stations[current].LineCode1 + '">' + marker.lines[0] + '</p>' +
-               '<p class="stationLine ' + model.Stations[current].LineCode2 + '">' + marker.lines[1] + '</p>' +
-               '<p class="stationLine ' + model.Stations[current].LineCode3 + '">' + marker.lines[2] + '</p>' +
-               '<p class="stationLine ' + model.Stations[current].LineCode4 + '">' + marker.lines[3] 
+               '<p class="stationLine ' + marker.lines[0] + '">' + marker.lines[0] + '</p>' +
+               '<p class="stationLine ' + marker.lines[1] + '">' + marker.lines[1] + '</p>' +
+               '<p class="stationLine ' + marker.lines[2] + '">' + marker.lines[2] + '</p>' +
+               '<p class="stationLine ' + marker.lines[3] + '">' + marker.lines[3] 
 
                //Gives each station an infowindow that displays the information above
                marker.infowindow = new google.maps.InfoWindow({
-                  content: contentString,
-                  maxWidth: 150
+                  content: contentString 
                });
 
                //If that marker's infoWindow is NOT open, it will open.
--- js/metroinfo.js
+++ js/metroinfo.js
@@ -1,23 +1,27 @@
 //Gets metro station data from WMATA API
 
-var trainData;
+// var trainData;
 
-function saveMetroData(){
-	trainData = JSON.parse(this.responseText);
-	console.log(trainData);
-}
+// function saveMetroData(){
+// 	trainData = JSON.parse(this.responseText);
+// 	console.log(trainData);
+// }
 
-function transferFailed(){
-	console.log('An error occured during the API call.');
-}
+// function transferFailed(){
+// 	console.log('An error occured during the API call.');
+// }
 
-function transferCanceled(){
-	console.log('The user canceled the request.');
-}
+// function transferCanceled(){
+// 	console.log('The user canceled the request.');
+// }
 
-var trainStationRequest = new XMLHttpRequest();
-trainStationRequest.addEventListener('load', saveMetroData);
-trainStationRequest.addEventListener("error", transferFailed);
-trainStationRequest.addEventListener("abort", transferCanceled);
-trainStationRequest.open('GET', 'https://api.wmata.com/Rail.svc/json/jStations?api_key=1371dab10bc845bea40ef0d8f9aae1cf', true);
-trainStationRequest.send();
+// function getRealTimePrediction(stationCode){
+// 	return 'https://api.wmata.com/StationPrediction.svc/json/GetPrediction/' + stationCode + '?api_key=1371dab10bc845bea40ef0d8f9aae1cf'
+// }
+
+// var trainStationRequest = new XMLHttpRequest();
+// trainStationRequest.addEventListener('load', saveMetroData);
+// trainStationRequest.addEventListener('error', transferFailed);
+// trainStationRequest.addEventListener('abort', transferCanceled);
+// trainStationRequest.open('GET', getRealTimePrediction('A04'), true);
+// trainStationRequest.send();
